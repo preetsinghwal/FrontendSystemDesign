e920226998ab384d5c2df2bff3e3f61956be4e7a
preetsinghwal <preetsinghwal38@gmail.com>
2026-08-29 19:38:23 +0530

    feat: Infinite scroll LLD

--- lowleveldesign/src/Body.js
+++ lowleveldesign/src/Body.js
@@ -3,23 +3,41 @@ import MemeCard from './MemeCard.js';
 import Shimmer from './Shimmer.js';
 
 const Body = function() {
-    const [memes, setMemes] = useState(null)
+    const [memes, setMemes] = useState([]);
+    const [showShimmer, setShowShimmer] = useState(false);
 
     useEffect(()=> {
         fetchMemes();
+
+        window.addEventListener('scroll', handleScroll);
+
+        return()=> window.removeEventListener('scroll', handleScroll);
     }, [])
+
+
+    const handleScroll = () => {
+        if(window.scrollY + window.innerHeight >= document.body.scrollHeight) {
+            fetchMemes();
+        }
+    }
+
     const fetchMemes = async () => {
+        setShowShimmer(true);
         const data = await fetch('https://meme-api.com/gimme/20');
         const parsedData = await data.json();
-        setMemes(parsedData.memes);
+
+        setShowShimmer(false);
+        setMemes((prevData)=> [...prevData, ...parsedData.memes] );
     }
     return (
         <>
         <div className="main-container">
 
-          {!memes ? ( <Shimmer /> ) : ( memes.map((data)=> (
+          {memes.map((data)=> (
             <MemeCard key={data.title} data={data} />
-          )))}
+          ))}
+
+          {showShimmer && <Shimmer />}
           
           </div>
         </>
